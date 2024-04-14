76beb124b651f5477b24d61afa74c6e889eede01
Marcel Baumgartner <marcel.kbkm@gmail.com>
2024-04-14 13:33:12 +0200

    Added error handling for offline server stats call

--- MoonlightDaemon/App/Extensions/ServerExtensions/ServerStatsExtension.cs
+++ MoonlightDaemon/App/Extensions/ServerExtensions/ServerStatsExtension.cs
@@ -8,31 +8,38 @@ public static class ServerStatsExtension
 {
     public static async Task<ServerStats> GetStats(this Server server)
     {
-        var client = server.ServiceProvider.GetRequiredService<DockerClient>();
-        var containerName = $"moonlight-runtime-{server.Configuration.Id}";
-
-        ServerStats result = new();
-        TaskCompletionSource completion = new();
-            
-        await client.Containers.GetContainerStatsAsync(containerName, new()
+        try
         {
-            Stream = false
-        }, new Progress<ContainerStatsResponse>(response =>
+            var client = server.ServiceProvider.GetRequiredService<DockerClient>();
+            var containerName = $"moonlight-runtime-{server.Configuration.Id}";
+
+            ServerStats result = new();
+            TaskCompletionSource completion = new();
+
+            await client.Containers.GetContainerStatsAsync(containerName, new()
+            {
+                Stream = false
+            }, new Progress<ContainerStatsResponse>(response =>
+            {
+                try
+                {
+                    result = response.ToServerStats();
+                    completion.SetResult();
+                }
+                catch (Exception)
+                {
+                    completion.SetResult();
+                }
+            }), CancellationToken.None);
+
+            await completion.Task;
+
+            return result;
+        }
+        catch (DockerContainerNotFoundException)
         {
-            try
-            {
-                result = response.ToServerStats();
-                completion.SetResult();
-            }
-            catch (Exception)
-            {
-                completion.SetResult();
-            }
-        }), CancellationToken.None);
-
-        await completion.Task;
-
-        return result;
+            return new();
+        }
     }
 
     public static async Task<CancellationTokenSource> GetStatsStream(this Server server, Func<ServerStats, Task> handle)
